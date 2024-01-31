91776a24fbc342366b4638f97c4fea19e9178254
Work <nwathnmala@gmail.com>
2024-02-01 01:49:18 +0700

    server

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
 });
 
 app.post('/register',(req,res)=>{
